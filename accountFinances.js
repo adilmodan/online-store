31c74ac26be8ca96618bd1574c933c6402c9b016
Jonathan Chen <58317363+jonathanchen1122@users.noreply.github.com>
2020-04-30 08:08:47 -0700

    Create accountFinances.js
    <button id = "addToCart()" >	   pay now	</button>

---
+++ accountFinances.js
@@ -0,0 +1,22 @@
+<script>
+
+var totalCostOwe = 0;
+var costNum = 0;
+
+
+<p id="ex#idToReplaceWords"></p> 
+
+
+function addToCart()	{
+
+totalCostOwe += costNum;    // adding product price to total owed cost
+
+document.getElementById("ex#idToReplaceWords").innerHTML = "This Item has been added to your cart";
+}
+
+If (button pressed)	{
+
+addToCart();
+}
+
+</script>
